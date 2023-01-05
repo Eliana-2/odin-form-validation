922b0bfd971b0c82a20f8c5d309e6b305eb026a5
Eliana-2 <eliana.guizar1@gmail.com>
2023-01-04 16:15:05 -0800

    Add password validation and error messages

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
 import './style.css';
-import {showError, clearError} from './modules/errorDisplay.js'
+import {validateInput} from './modules/errorDisplay.js'
 const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
-
-function validateInput(input) {
-  clearError(input);
-  if(input.validity.valueMissing) {showError(input, 'Required Field')}
-  else if (input.validity.tooShort) {showError(input, input.previousElementSibling.textContent + ' must be at least ' + input.minLength + ' characters')}
-  else {
-    switch(input.id)
-    {
-      case 'email':
-        if(input.validity.typeMismatch) {showError(input, 'Invalid Email')}
-        break;
-      case 'country':
-        if(input.validity.patternMismatch) {showError(input, 'Country must consist of letters and spaces')}
-        break;
-      case 'zip_code':
-        if(input.validity.patternMismatch) {showError(input, 'Zip Code must contain 5 digits')}
-        break;
-  }}
-}
 
 inputs.forEach((input) => {
   input.addEventListener('input', () => {
@@ -30,4 +11,7 @@ inputs.forEach((input) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  inputs.forEach((input) => {
+    validateInput(input);
+  })
 })
--- modules/errorDisplay.js
+++ modules/errorDisplay.js
@@ -10,4 +10,38 @@ function clearError(input) {
   input.classList.remove('error');
 }
 
-export {showError, clearError};
+function isValidPassword(password) {
+  if(password.match('[^A-Za-z0-9!@#$%^&*]') !== null) {
+    return false}
+  else if (password.match('[A-Z]') === null) {return false}
+  else if (password.match('[\d]') === null) {return false}
+  else if (password.match('[!@#$%^&*]') === null) {return false}
+  else {return true}
+}
+
+function validateInput(input) {
+  clearError(input);
+  if(input.validity.valueMissing) {showError(input, 'Required Field')}
+  else if (input.validity.tooShort) {showError(input, input.previousElementSibling.textContent + ' must be at least ' + input.minLength + ' characters')}
+  else {
+    switch(input.id)
+    {
+      case 'email':
+        if(input.validity.typeMismatch) {showError(input, 'Invalid Email')}
+        break;
+      case 'country':
+        if(input.validity.patternMismatch) {showError(input, 'Country must consist of letters and spaces')}
+        break;
+      case 'zip_code':
+        if(input.validity.patternMismatch) {showError(input, 'Zip Code must contain 5 digits')}
+        break;
+      case 'password':
+        if(!(isValidPassword(input.value))) {showError(input, 'Password must contain at least one capital letter, one number, and one special character')}
+        break;
+      case 'password_confirmation':
+        if(input.value !== document.getElementById('password').value) {showError(input, 'Passwords must match')}
+        break;
+  }}
+}
+
+export {validateInput};
